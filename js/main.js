1ad520284c1ee10f76f752f3b1ca43c56e096294
muac404 <muac404@gmail.com>
2022-11-03 12:49:36 +0100

    Aggiunge condizione difficoltà

--- js/main.js
+++ js/main.js
@@ -1,20 +1,67 @@
 "use strict"
 
 const playBtn = document.getElementById("play-btn")
-playBtn.addEventListener("click", function(){
-    const boardContainer = document.getElementById("container")
-    boardContainer.innerHTML = ""
-    for (let i = 1; i <= 100; i++) {
-        const boardCell = document.createElement("div");
-        boardCell.innerHTML = i
-        boardCell.classList.add("cell-grid")
-        boardContainer.append(boardCell)
-        boardCell.addEventListener("click", function(){
-            console.log(this.innerHTML)
-            this.classList.add("blue-bg")
-        })
-    }
-})
+
+// let option = ["easy", "medium", "hard"];
+let userOption = document.getElementById("difficult").value
+
+
+console.log(userOption)
+
+
+
+
+if (userOption == "easy"){
+    playBtn.addEventListener("click", function(){
+        const boardContainer = document.getElementById("container")
+        boardContainer.innerHTML = ""
+        for (let i = 1; i <= 100; i++) {
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i
+            boardCell.classList.add("cell-grid")
+            boardContainer.append(boardCell)
+            boardCell.addEventListener("click", function(){
+                console.log(this.innerHTML)
+                this.classList.add("blue-bg")
+            })
+        }
+    })
+}
+
+if (userOption == "medium"){
+    playBtn.addEventListener("click", function(){
+        const boardContainer = document.getElementById("container")
+        boardContainer.innerHTML = ""
+        for (let i = 1; i <= 81; i++) {
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i
+            boardCell.classList.add("cell-grid")
+            boardContainer.append(boardCell)
+            boardCell.addEventListener("click", function(){
+                console.log(this.innerHTML)
+                this.classList.add("blue-bg")
+            })
+        }
+    })
+}
+
+if (userOption == "hard"){
+    playBtn.addEventListener("click", function(){
+        const boardContainer = document.getElementById("container")
+        boardContainer.innerHTML = ""
+        for (let i = 1; i <= 49; i++) {
+            const boardCell = document.createElement("div");
+            boardCell.innerHTML = i
+            boardCell.classList.add("cell-grid")
+            boardContainer.append(boardCell)
+            boardCell.addEventListener("click", function(){
+                console.log(this.innerHTML)
+                this.classList.add("blue-bg")
+            })
+        }
+    })
+}
+
 
 
 
